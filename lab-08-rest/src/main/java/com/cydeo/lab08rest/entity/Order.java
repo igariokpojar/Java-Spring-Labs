3e9a1bf39343725a75d91d0c52aed2119f5a3cfe
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-12-12 18:50:58 -0600

    Lab

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/entity/Order.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/entity/Order.java
@@ -18,6 +18,11 @@ public class Order extends BaseEntity{
     private BigDecimal totalPrice;
     @ManyToOne
     private Customer customer;
+
     @OneToOne(cascade = CascadeType.ALL) // (cascade = CascadeType.ALL)->this is for Transient error
-    private Payment payment;
+    private Payment payment;/*
+    You have to put this cascade all out to your parent tables. Then, when you try to figure out when you try to put or change the data in different from different table, then it's gonna affect all
+things. you know the main purpose of cascade. All was. If you change something you need to change related database
+field, right? So that's why it's giving error, and it's Push me to put cascade all fields
+     */
 }
